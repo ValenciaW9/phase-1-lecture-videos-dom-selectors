b9341ed4b3c8189c33793fc9be6e1934c5b3fd30
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-14 21:32:36 -0400

    phase-1-lecture-videos-dom-selectors/
    phase-1-lecture-videos-dom-selectors/

---
+++ phase-0-the-dom-modifying-elements-lab-master/index.js
@@ -0,0 +1,18 @@
+function textContent(rootNode) {
+    if ('textContent' in document.createTextNode(''))
+      return rootNode.textContent;
+  
+    var childNodes = rootNode.childNodes,
+        len = childNodes.length,
+        result = '';
+    
+    for (var i = 0; i < len; i++) {
+      if (childNodes[i].nodeType === 3)
+        result += childNodes[i].nodeValue;
+      else if (childNodes[i].nodeType === 1) 
+        result += textContent(childNodes[i]);
+    }
+  
+    return result;
+  }
+  
